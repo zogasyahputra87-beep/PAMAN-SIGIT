24530440fdd8b94e66b20c60e014fa2792176236
zogasyahputra87-beep <zogasyahputra87@gmail.com>
2026-04-15 15:38:05 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'sicodi-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/buat-konsultasi.html',
+  '/riwayat.html',
+  '/login.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
